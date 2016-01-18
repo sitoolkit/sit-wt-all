7c83ee40bf04a35372a55a4551c02294a284f109
Yuichi Kuwahara <yuichi.kuwahara.0@gmail.com>
2016-01-18 14:26:14 +0900

    selenium2scriptのdefaultphaseを追加

--- sit-wt-maven-plugin/src/main/java/org/sitoolkit/wt/plugin/maven/Selenium2ScriptMojo.java
+++ sit-wt-maven-plugin/src/main/java/org/sitoolkit/wt/plugin/maven/Selenium2ScriptMojo.java
@@ -2,11 +2,12 @@ package org.sitoolkit.wt.plugin.maven;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.sitoolkit.wt.app.selenium2script.Selenium2Script;
 
-@Mojo(name = "selenium2script", requiresDependencyCollection = ResolutionScope.RUNTIME )
+@Mojo(name = "selenium2script", defaultPhase = LifecyclePhase.GENERATE_TEST_SOURCES, requiresDependencyCollection = ResolutionScope.RUNTIME)
 public class Selenium2ScriptMojo extends AbstractMojo {
 
     @Override
