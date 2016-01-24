ef249fd79f43898a47644b7e9e82a1c3d5ba3d53
Yuichi Kuwahara <yuichi.kuwahara.0@gmail.com>
2016-01-24 22:04:33 +0900

    evidence openを抑止するフラグを追加

--- sit-wt-maven-plugin/src/main/java/org/sitoolkit/wt/plugin/maven/EvidenceOpenMojo.java
+++ sit-wt-maven-plugin/src/main/java/org/sitoolkit/wt/plugin/maven/EvidenceOpenMojo.java
@@ -5,15 +5,21 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.sitoolkit.wt.domain.evidence.EvidenceOpener;
 
 @Mojo(name = "open-evidence", defaultPhase = LifecyclePhase.POST_INTEGRATION_TEST)
 public class EvidenceOpenMojo extends AbstractMojo {
 
+    @Parameter(property = "evidence.open", defaultValue = "true")
+    private String evidenceOpen;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        EvidenceOpener opener = new EvidenceOpener();
-        opener.open();
+        if (Boolean.parseBoolean(evidenceOpen)) {
+            EvidenceOpener opener = new EvidenceOpener();
+            opener.open();
+        }
     }
 
 }
