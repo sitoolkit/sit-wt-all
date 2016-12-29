7fe7b5b862ab8eca880779943b50c9fd79e9fea9
Yuichi Kuwahara <yuichi.kuwahara.0@gmail.com>
2016-12-29 15:50:02 +0900

    アイコンを追加

--- sit-wt-app/src/main/java/org/sitoolkit/wt/gui/pres/App.java
+++ sit-wt-app/src/main/java/org/sitoolkit/wt/gui/pres/App.java
@@ -16,6 +16,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
@@ -63,6 +64,7 @@ public class App extends Application {
 
         scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
         primaryStage.setScene(scene);
+        primaryStage.getIcons().add(new Image("/icon/sitoolkit.png"));
         primaryStage.show();
 
     }
