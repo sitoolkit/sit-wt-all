4a1992e13931737042a4295cf6bf6b22f8540478
Kazunari Fujinuma <monocrea.fujinuma@DESKTOP-20M0B3C>
2018-08-03 18:47:09 +0900

    文字コードごとのCSV読書きテストコード（JUNIT）を追加

--- sit-wt-runtime/src/test/java/org/sitoolkit/wt/infra/csv/CsvFileReaderTest.java
+++ sit-wt-runtime/src/test/java/org/sitoolkit/wt/infra/csv/CsvFileReaderTest.java
@@ -3,13 +3,25 @@ package org.sitoolkit.wt.infra.csv;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.IntStream;
 
-import org.junit.Test;
+import javax.annotation.Resource;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sitoolkit.wt.app.config.BaseConfig;
+import org.sitoolkit.wt.infra.PropertyManager;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = BaseConfig.class)
 public class CsvFileReaderTest {
 
     private String[] columns = new String[] { "No.", "項目名", "操作", "ロケーター形式", "ロケーター", "データ形式",
@@ -25,18 +37,21 @@ public class CsvFileReaderTest {
     private Map<String, String> expectedFooter = new LinkedHashMap<>();
 
     public CsvFileReaderTest() {
-
         IntStream.range(0, expectedHeaderData.length).forEachOrdered(i -> {
             expectedHeader.put(columns[i], expectedHeaderData[i]);
             expectedFooter.put(columns[i], expectedFooterData[i]);
         });
-
     }
+
+    @Resource
+    CsvFileReader target;
+
+    @Resource
+    PropertyManager pm;
 
     @Test
     public void test() {
-        CsvFileReader target = new CsvFileReader();
-        List<Map<String, String>> result = target.read("testscript/CsvTestScript.csv", false);
+        List<Map<String, String>> result = target.read("testdata/csv/CsvTestScriptBom.csv", false);
         assertThat(result.size(), is(17));
         assertThat(result.get(0), is(expectedHeader));
         assertThat(result.get(16), is(expectedFooter));
@@ -44,18 +59,42 @@ public class CsvFileReaderTest {
 
     @Test
     public void testHeaderOnly() {
-        CsvFileReader target = new CsvFileReader();
-        List<Map<String, String>> result = target.read("testscript/CsvTestScript.csv", true);
+        List<Map<String, String>> result = target.read("testdata/csv/CsvTestScriptBom.csv", true);
         assertThat(result.size(), is(1));
         assertThat(result.get(0), is(expectedHeader));
     }
 
     @Test(expected = IllegalStateException.class)
     public void testException() {
-        CsvFileReader target = new CsvFileReader();
-
         @SuppressWarnings("unused")
         List<Map<String, String>> result = target.read("illegalPath", false);
+    }
+
+    @Test
+    public void testNoBom() {
+        pm.setCsvCharset(StandardCharsets.UTF_8);
+        pm.setCsvHasBOM(false);
+        List<Map<String, String>> result = target.read("testdata/csv/CsvTestScriptNoBom.csv",
+                false);
+        assertThat(result.size(), is(17));
+        assertThat(result.get(0), is(expectedHeader));
+        assertThat(result.get(16), is(expectedFooter));
+    }
+
+    @Test
+    public void testSJIS() {
+        pm.setCsvCharset(Charset.forName("Windows-31j"));
+        pm.setCsvHasBOM(false);
+        List<Map<String, String>> result = target.read("testdata/csv/CsvTestScriptSJIS.csv", false);
+        assertThat(result.size(), is(17));
+        assertThat(result.get(0), is(expectedHeader));
+        assertThat(result.get(16), is(expectedFooter));
+    }
+
+    @Before
+    public void setup() {
+        pm.setCsvCharset(StandardCharsets.UTF_8);
+        pm.setCsvHasBOM(true);
     }
 
 }
--- sit-wt-runtime/src/test/java/org/sitoolkit/wt/infra/csv/CsvFileWriterTest.java
+++ sit-wt-runtime/src/test/java/org/sitoolkit/wt/infra/csv/CsvFileWriterTest.java
@@ -4,7 +4,9 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -12,18 +14,78 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.Test;
+import javax.annotation.Resource;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sitoolkit.wt.app.config.BaseConfig;
+import org.sitoolkit.wt.infra.PropertyManager;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = BaseConfig.class)
 public class CsvFileWriterTest {
+
+    @Resource
+    CsvFileWriter target;
+
+    @Resource
+    PropertyManager pm;
+
+    List<List<String>> data = new ArrayList<>();
 
     @Test
     public void test() throws IOException {
 
-        CsvFileWriter target = new CsvFileWriter();
+        Path actualPath = Paths.get("target/CsvWriteTestActual.csv");
+        target.write(data, actualPath.toAbsolutePath().toString());
+        Path expectedPath = Paths.get("testdata/csv/CsvWriteTestExpectedBom.csv");
 
-        List<List<String>> data = new ArrayList<>();
+        List<String> actualAllLines = FileUtils.readLines(actualPath.toFile(),
+                StandardCharsets.UTF_8);
+        Iterator<String> actualItr = actualAllLines.iterator();
 
+        List<String> expectedAllLines = FileUtils.readLines(expectedPath.toFile(),
+                StandardCharsets.UTF_8);
+
+        assertThat(actualAllLines.size(), is(expectedAllLines.size()));
+
+        for (Iterator<String> expectedItr = expectedAllLines.iterator(); expectedItr.hasNext();) {
+            assertThat(actualItr.next(), is(expectedItr.next()));
+        }
+
+        assertThat(Files.readAllBytes(actualPath), is(Files.readAllBytes(expectedPath)));
+    }
+
+    @Test
+    public void testNoBom() throws IOException {
+        pm.setCsvCharset(StandardCharsets.UTF_8);
+        pm.setCsvHasBOM(false);
+
+        Path actualPath = Paths.get("target/CsvWriteTestActual.csv");
+        target.write(data, actualPath.toAbsolutePath().toString());
+        Path expectedPath = Paths.get("testdata/csv/CsvWriteTestExpectedNoBom.csv");
+
+        assertThat(Files.readAllBytes(actualPath), is(Files.readAllBytes(expectedPath)));
+    }
+
+    @Test
+    public void testSJIS() throws IOException {
+        pm.setCsvCharset(Charset.forName("Windows-31j"));
+        pm.setCsvHasBOM(false);
+
+        Path actualPath = Paths.get("target/CsvWriteTestActual.csv");
+        target.write(data, actualPath.toAbsolutePath().toString());
+        Path expectedPath = Paths.get("testdata/csv/CsvWriteTestExpectedSJIS.csv");
+
+        assertThat(Files.readAllBytes(actualPath), is(Files.readAllBytes(expectedPath)));
+    }
+
+    @Before
+    public void setup() {
         data.add(Arrays.asList("正常データ", "12", "13", "14", "15"));
         data.add(Arrays.asList("途中列改行データ", "22", "2\n3", "24", "25"));
         data.add(Arrays.asList("最終列改行データ", "32", "33", "34", "3\n5"));
@@ -37,23 +99,8 @@ public class CsvFileWriterTest {
         data.add(Arrays.asList("ダブルクォートあり", "最後だけ\"", "\"真\"ん中\"", "\"最初だけ", "真中\"だけ"));
         data.add(Arrays.asList("ダブルクォートだけ", "\"", "\"\"", "\"\"\"", "\"\"\"\""));
 
-        Path actualPath = Paths.get("target/CsvWriteTestActual.csv");
-
-        target.write(data, actualPath.toAbsolutePath().toString());
-
-        List<String> actualAllLines = FileUtils.readLines(actualPath.toFile(),
-                StandardCharsets.UTF_8);
-        Iterator<String> actualItr = actualAllLines.iterator();
-
-        Path expectedPath = Paths.get("testdata/csv/CsvWriteTestExpected.csv");
-        List<String> expectedAllLines = FileUtils.readLines(expectedPath.toFile(),
-                StandardCharsets.UTF_8);
-
-        assertThat(actualAllLines.size(), is(expectedAllLines.size()));
-
-        for (Iterator<String> expectedItr = expectedAllLines.iterator(); expectedItr.hasNext();) {
-            assertThat(actualItr.next(), is(expectedItr.next()));
-        }
+        pm.setCsvCharset(StandardCharsets.UTF_8);
+        pm.setCsvHasBOM(true);
 
     }
 
