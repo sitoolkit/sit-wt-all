a783b03fdb5186c3030e906af5a63acee9d7f387
MasaoBlue <masao.hirocho@monocrea.jp>
2019-04-08 09:51:21 +0900

    Change template name

--- sit-wt-runtime/src/main/java/io/sitoolkit/wt/app/sample/SampleManager.java
+++ sit-wt-runtime/src/main/java/io/sitoolkit/wt/app/sample/SampleManager.java
@@ -4,15 +4,17 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStreamWriter;
+import java.io.UncheckedIOException;
 import java.net.URL;
-import java.nio.file.Path;
+import java.nio.charset.Charset;
 import java.util.Locale;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
-import org.apache.velocity.Template;
+import org.apache.commons.io.IOUtils;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.springframework.util.ResourceUtils;
@@ -25,27 +27,13 @@ public class SampleManager {
 
     private String projectDir = null;
 
-    public SampleManager() {
-    }
-
-    private void unarchive(String resource, File dest) {
-        URL res = ClassLoader.getSystemResource(resource);
-
-        try {
-            res = ResourceUtils.getURL("classpath:" + resource);
-            FileUtils.copyInputStreamToFile(res.openStream(), dest);
-        } catch (IOException e) {
-            throw new ConfigurationException(e);
-        }
-    }
-
     public void unarchiveBasicSample() {
         unarchive("sample/bootstrap.min.css");
         unarchive("sample/pom.xml");
 
-        Properties inputProperties = unarchiveAndApplyProperties("input.html");
-        Properties termsProperties = unarchiveAndApplyProperties("terms.html");
-        Properties doneProperties = unarchiveAndApplyProperties("done.html");
+        Properties inputProperties = createLocalizeFile("input.html");
+        Properties termsProperties = createLocalizeFile("terms.html");
+        Properties doneProperties = createLocalizeFile("done.html");
 
         Properties scriptProperties = loadProperties("CsvTestScript");
         scriptProperties.putAll(inputProperties);
@@ -53,48 +41,51 @@ public class SampleManager {
         scriptProperties.putAll(doneProperties);
 
         File scriptTemplateDest = new File(getDestDir("testscript"), "SampleTestScript.csv");
-        unarchive("sample/CsvTestScript_template.csv", scriptTemplateDest);
-
-        applyProperties(scriptTemplateDest.toPath(), scriptTemplateDest.toString(),
+        applyProperties(resource2str("sample/CsvTestScript.csv"), scriptTemplateDest.toString(),
                 scriptProperties);
     }
 
-    private Properties unarchiveAndApplyProperties(String filename) {
+    private Properties createLocalizeFile(String filename) {
         String name = FilenameUtils.removeExtension(filename);
-        String extension = FilenameUtils.getExtension(filename);
-        String templateResource = "sample/" + name + "_template." + extension;
-        // String templateDest = getDestDir(".") + "/" + templateResource;
-        String dest = getDestDir(".") + "/sample/" + filename;
+        String templateResource = "sample/" + filename;
+        String dest = getDestDir("") + templateResource;
 
-        Path templatePath = unarchive(templateResource);
         Properties properties = loadProperties(name);
-        return applyProperties(templatePath, dest, properties);
+        applyProperties(resource2str(templateResource), dest, properties);
+        return properties;
     }
 
-    private Properties applyProperties(Path templatePath, String dest, Properties properties) {
+    private void applyProperties(String template, String dest, Properties properties) {
         VelocityContext context = new VelocityContext(properties);
 
         try (FileOutputStream fos = new FileOutputStream(new File(dest));
                 OutputStreamWriter osw = new OutputStreamWriter(fos, CHARSET);
                 BufferedWriter bw = new BufferedWriter(osw);) {
 
-            VelocityEngine velocity = new VelocityEngine();
-            Template velocityTemplate = velocity.getTemplate(templatePath.toString(), CHARSET);
-            velocityTemplate.merge(context, bw);
+            new VelocityEngine().evaluate(context, bw, "name", template);
 
-            // Velocity.evaluate(context, bw, "name", "${input-page-title}");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        return properties;
     }
 
-    private Path unarchive(String resource) {
-        String[] dest = resource.split("/");
-        File destFile = new File(getDestDir(dest[0]), dest[1]);
-        unarchive(resource, destFile);
-        return destFile.toPath();
+    private void unarchive(String resource) {
+        File destFile = new File(getDestDir(resource));
+        try {
+            URL url = ResourceUtils.getURL("classpath:" + resource);
+            FileUtils.copyInputStreamToFile(url.openStream(), destFile);
+        } catch (IOException e) {
+            throw new ConfigurationException(e);
+        }
+    }
+
+    private String resource2str(String resource) {
+        try {
+            InputStream stream = this.getClass().getClassLoader().getResourceAsStream(resource);
+            return IOUtils.toString(stream, Charset.defaultCharset());
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     private Properties loadProperties(String name) {
