863c51dd40ccffd0320cb3f9862bba484c1b548c
MasaoBlue <masao.hirocho@monocrea.jp>
2019-05-13 11:02:26 +0900

    Change place of attach evidence link

--- sit-wt-runtime/src/main/resources/evidence/js/report.js
+++ sit-wt-runtime/src/main/resources/evidence/js/report.js
@@ -5,19 +5,20 @@ $(function() {
     return $("<a />")
       .attr("href", evidenceFile)
       .attr("target", "_blank")
-      .text("evidence");
+      .css("float", "right")
+      .text("Open evidence");
   }
 
   $("h2:contains(Test Cases)").closest(".section").find("tr").each(function (i, trElm) {
     let tr = $(trElm);
-    let td = $("<td />").appendTo(tr);
 
-    let testCaseLink = tr.find("td:nth-child(2) > a");
+    let testCaseTd = tr.find("td:nth-child(2)");
+    let testCaseLink = testCaseTd.find("> a");
     if (testCaseLink.length == 0) {
       return;
     }
 
     let caseName = testCaseLink.attr("name");
-    buildEvidenceLink(caseName).appendTo(td);
+    buildEvidenceLink(caseName).appendTo(testCaseTd);
   })
 });
