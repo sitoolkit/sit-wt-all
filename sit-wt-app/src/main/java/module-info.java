2219d80a26bc44917322feb6ba90b61ecec0906f
MasaoBlue <masao.hirocho@monocrea.jp>
2019-07-31 13:36:59 +0900

    Enable eclipse debug

--- sit-wt-app/src/main/java/module-info.java
+++ sit-wt-app/src/main/java/module-info.java
@@ -1,23 +1,4 @@
 module sit.wt.app {
-  requires commons.io;
-  requires commons.lang3;
-  requires controlsfx;
-  requires javafx.web;
-  requires javafx.fxml;
-  requires java.annotation;
-  requires spring.context;
-  requires spring.core;
-  requires spring.beans;
-  requires selenium.support;
-  requires lombok;
-  requires org.mapstruct;
-  requires org.mapstruct.processor;
-  requires sit.wt.util;
-  requires sit.wt.runtime;
-  requires sit.util.bth;
-  requires logback.core;
-  requires slf4j.api;
-
   requires java.xml;
   requires jdk.unsupported;
   requires java.sql;
@@ -26,9 +7,34 @@ module sit.wt.app {
   requires java.management;
   requires java.security.jgss;
   requires java.instrument;
+  requires javafx.web;
+  requires javafx.fxml;
+  requires java.annotation;
+  requires spring.context;
+  requires spring.core;
+  requires spring.beans;
+  requires lombok;
+  requires org.mapstruct;
+  requires org.mapstruct.processor;
+  requires commons.io;
+  requires commons.lang3;
+  requires controlsfx;
+  requires selenium.support;
+  requires logback.core;
+  requires slf4j.api;
 
+  requires sit.wt.util;
+  requires sit.wt.runtime;
+  requires sit.util.bth;
 
-  opens io.sitoolkit.wt.gui.pres to javafx.fxml;
+  opens io.sitoolkit.wt.gui.pres to javafx.fxml, spring.core;
+  opens io.sitoolkit.wt.gui.infra.config to spring.core, spring.beans, spring.context;
+  opens io.sitoolkit.wt.gui.app.script to spring.core;
+  opens io.sitoolkit.wt.gui.app.sample to spring.core;
+  opens io.sitoolkit.wt.gui.app.project to spring.core;
+  opens io.sitoolkit.wt.gui.app.diffevidence to spring.core;
+  opens io.sitoolkit.wt.gui.app.test to spring.core;
+  opens io.sitoolkit.wt.gui.app.update to spring.core;
 
   exports io.sitoolkit.wt.gui.pres;
 }
