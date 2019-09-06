397a3c9bb156209fe5e98ee151460562810bfa1b
MasaoBlue <masao.hirocho@monocrea.jp>
2019-09-06 16:32:58 +0900

    Modify launch method name

--- sit-wt-app/src/main/java/io/sitoolkit/wt/gui/pres/SitWtLauncher.java
+++ sit-wt-app/src/main/java/io/sitoolkit/wt/gui/pres/SitWtLauncher.java
@@ -4,6 +4,6 @@ package io.sitoolkit.wt.gui.pres;
 public class SitWtLauncher {
 
   public static void main(String[] args) {
-    SitWtApplication.main(args);
+    SitWtApplication.launchApplication(args);
   }
 }
