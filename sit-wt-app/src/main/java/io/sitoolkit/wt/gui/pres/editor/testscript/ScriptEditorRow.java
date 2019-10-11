23c73d9fab6832d41605c5d9502f7580205cf743
kfujinuma <40159739+kfujinuma@users.noreply.github.com>
2019-10-11 15:51:06 +0900

    Feature/tableview edit script (#66)

---
+++ sit-wt-app/src/main/java/io/sitoolkit/wt/gui/pres/editor/testscript/ScriptEditorRow.java
@@ -0,0 +1,117 @@
+package io.sitoolkit.wt.gui.pres.editor.testscript;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import io.sitoolkit.wt.domain.testscript.Locator;
+import io.sitoolkit.wt.domain.testscript.TestStep;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class ScriptEditorRow {
+
+  private StringProperty no;
+  private StringProperty itemName;
+  private StringProperty operationName;
+  private StringProperty locatorType;
+  private StringProperty locator;
+  private StringProperty dataType;
+  private StringProperty screenshotTiming;
+  private StringProperty breakpoint; // TODO move cell to row-selector
+
+  // caseName -> property of testData
+  private Map<String, StringProperty> testData = new HashMap<>();
+
+  public static ScriptEditorRow createFromTestStep(TestStep testStep) {
+    ScriptEditorRow row = new ScriptEditorRow();
+    row.loadTestStep(testStep);
+    return row;
+  }
+
+  private void loadTestStep(TestStep testStep) {
+    noProperty().setValue(testStep.getNo());
+    itemNameProperty().setValue(testStep.getItemName());
+    operationNameProperty().setValue(testStep.getOperationName());
+    locatorTypeProperty().setValue(testStep.getLocator().getType());
+    locatorProperty().setValue(testStep.getLocator().getValue());
+    dataTypeProperty().setValue(testStep.getDataType());
+    screenshotTimingProperty().setValue(testStep.getScreenshotTiming().getLabel());
+    breakpointProperty().setValue(testStep.getBreakPoint());
+
+    for (Entry<String, String> entry : testStep.getTestData().entrySet()) {
+      testDataProperty(entry.getKey()).setValue(entry.getValue());
+    }
+  }
+
+  public TestStep buildTestStep(List<String> caseNoList) {
+    TestStep testStep = new TestStep();
+    testStep.setNo(noProperty().getValue());
+    testStep.setItemName(itemNameProperty().getValue());
+    testStep.setOperationName(operationNameProperty().getValue());
+    Locator locator = new Locator();
+    locator.setType(locatorTypeProperty().getValue());
+    locator.setValue(locatorProperty().getValue());
+    testStep.setLocator(locator);
+    testStep.setDataType(dataTypeProperty().getValue());
+    testStep.setScreenshotTiming(screenshotTimingProperty().getValue());
+    testStep.setBreakPoint(breakpointProperty().getValue());
+
+    Map<String, String> testData = new LinkedHashMap<String, String>();
+    for (String caseNo : caseNoList) {
+      testData.put(caseNo, testDataProperty(caseNo).getValue());
+    }
+    testStep.setTestData(testData);
+
+    return testStep;
+  }
+
+  public StringProperty noProperty() {
+    if (no == null) no = new SimpleStringProperty(this, "no");
+    return no;
+  }
+
+  public StringProperty itemNameProperty() {
+    if (itemName == null) itemName = new SimpleStringProperty(this, "itemName");
+    return itemName;
+  }
+
+  public StringProperty operationNameProperty() {
+    if (operationName == null) operationName = new SimpleStringProperty(this, "operationName");
+    return operationName;
+  }
+
+  public StringProperty locatorTypeProperty() {
+    if (locatorType == null) locatorType = new SimpleStringProperty(this, "locatorType");
+    return locatorType;
+  }
+
+  public StringProperty locatorProperty() {
+    if (locator == null) locator = new SimpleStringProperty(this, "locator");
+    return locator;
+  }
+
+  public StringProperty dataTypeProperty() {
+    if (dataType == null) dataType = new SimpleStringProperty(this, "dataType");
+    return dataType;
+  }
+
+  public StringProperty screenshotTimingProperty() {
+    if (screenshotTiming == null)
+      screenshotTiming = new SimpleStringProperty(this, "screenshotTiming");
+    return screenshotTiming;
+  }
+
+  public StringProperty breakpointProperty() {
+    if (breakpoint == null) breakpoint = new SimpleStringProperty(this, "breakpoint");
+    return breakpoint;
+  }
+
+  public StringProperty testDataProperty(String caseNo) {
+    if (testData.get(caseNo) == null) {
+      testData.put(caseNo, new SimpleStringProperty(this, "case_" + caseNo));
+    }
+    return testData.get(caseNo);
+  }
+}
---
+++ sit-wt-app/src/main/java/io/sitoolkit/wt/gui/pres/editor/testscript/TestScriptEditorFxImpl.java
@@ -0,0 +1,252 @@
+package io.sitoolkit.wt.gui.pres.editor.testscript;
+
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.controlsfx.control.spreadsheet.GridChange;
+import io.sitoolkit.wt.domain.testscript.TestScript;
+import io.sitoolkit.wt.domain.testscript.TestStep;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.util.Callback;
+
+public class TestScriptEditorFxImpl implements TestScriptEditor {
+
+  private static final int COLUMN_INDEX_FIRST_CASE = 8;
+
+  private TableView<ScriptEditorRow> tableView = new TableView<>();
+
+  @Override
+  public void load(TestScript testScript) {
+    tableView.setItems(buildEditorRows(testScript));
+    tableView.getColumns().setAll(buildEditorColumns(testScript));
+    tableView.setEditable(true);
+
+    tableView.setId(testScript.getScriptFile().getAbsolutePath());
+    tableView
+        .getStylesheets()
+        .add(getClass().getResource("/testScriptEditor.css").toExternalForm());
+
+    // TODO implement SpreadSheet's RowPickers-like feature instead of breakpoint column
+    // TODO set context-menu-event-handler to tableView
+    //    tableView.setOnContextMenuRequested(new ContextMenuEventHandler());
+  }
+
+  private List<TableColumn<ScriptEditorRow, String>> buildEditorColumns(TestScript testScript) {
+    List<TableColumn<ScriptEditorRow, String>> columns = new ArrayList<>();
+    int colIndex = 0;
+    for (String headerName : testScript.getHeaders()) {
+      String caseNo = getCaseNo(testScript, headerName);
+      columns.add(buildEditorColumn(headerName, colIndex, caseNo));
+      colIndex++;
+    }
+    return columns;
+  }
+
+  private TableColumn<ScriptEditorRow, String> buildEditorColumn(
+      String headerName, int columnIndex, String caseNo) {
+    TableColumn<ScriptEditorRow, String> col = new TableColumn<>(headerName);
+    col.setCellFactory(TextFieldTableCell.forTableColumn());
+    col.setCellValueFactory(createCellValueFactory(columnIndex, caseNo));
+    col.setEditable(true);
+    return col;
+  }
+
+  private String getCaseNo(TestScript testScript, String headerName) {
+    return StringUtils.substringAfter(headerName, testScript.getCaseNoPrefix());
+  }
+
+  private Callback<CellDataFeatures<ScriptEditorRow, String>, ObservableValue<String>>
+      createCellValueFactory(int columnIndex, String caseNo) {
+
+    switch (columnIndex) {
+      case 0:
+        return p -> p.getValue().noProperty();
+      case 1:
+        return p -> p.getValue().itemNameProperty();
+      case 2:
+        return p -> p.getValue().operationNameProperty();
+      case 3:
+        return p -> p.getValue().locatorTypeProperty();
+      case 4:
+        return p -> p.getValue().locatorProperty();
+      case 5:
+        return p -> p.getValue().dataTypeProperty();
+      case 6:
+        return p -> p.getValue().screenshotTimingProperty();
+      case 7:
+        return p -> p.getValue().breakpointProperty();
+      default:
+        return p -> p.getValue().testDataProperty(caseNo);
+    }
+  }
+
+  private ObservableList<ScriptEditorRow> buildEditorRows(TestScript testScript) {
+    return testScript
+        .getTestStepList()
+        .stream()
+        .map(ScriptEditorRow::createFromTestStep)
+        .collect(toCollection(FXCollections::observableArrayList));
+  }
+
+  @Override
+  public ContextMenu getContextMenu() {
+    // TODO Auto-generated method stub
+    return new ContextMenu();
+  }
+
+  @Override
+  public TestScript buildTestScript() {
+    TestScript testScript = new TestScript();
+    testScript.setScriptFile(new File(tableView.getId()));
+    List<String> headers =
+        tableView.getColumns().stream().map(TableColumn::getText).collect(toList());
+    List<String> caseNoList =
+        headers
+            .stream()
+            .skip(COLUMN_INDEX_FIRST_CASE)
+            .map(header -> getCaseNo(testScript, header))
+            .collect(toList());
+    List<TestStep> testStepList =
+        tableView.getItems().stream().map(row -> row.buildTestStep(caseNoList)).collect(toList());
+
+    headers.stream().forEach(header -> testScript.addHeader(header));
+    testScript.setTestStepList(testStepList);
+    return testScript;
+  }
+
+  @Override
+  public void setDebugStyle(int nextStepIndex, int caseIndex) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void removeDebugStyle() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean insertTestCase() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void appendTestCase() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean insertTestStep() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void appendTestStep() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public int getCaseCount(List<GridChange> changeList) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public boolean insertTestCases(int count) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void pasteClipboard() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public int getStepCount(List<GridChange> changeList) {
+    // TODO Auto-generated method stub
+    return 0;
+  }
+
+  @Override
+  public boolean insertTestSteps(int count) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void appendTestCases(int count) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void appendTestSteps(int count) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void deleteTestCase() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void deleteTestStep() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public boolean isCaseSelected() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean isStepSelected() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean isCaseInsertable() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public boolean isStepInsertable() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public void toggleBreakpoint() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public Node getSpreadSheet() {
+    return tableView;
+  }
+}
