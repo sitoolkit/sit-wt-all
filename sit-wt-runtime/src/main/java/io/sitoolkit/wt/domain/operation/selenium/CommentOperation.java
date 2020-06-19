952c39347e58f73dcfd8e2c9b4c161ff56cabba2
Hirocho <m.hirocho@accenture.com>
2020-06-19 16:36:01 +0900

    Add empty locator type

--- sit-wt-runtime/src/main/java/io/sitoolkit/wt/domain/operation/selenium/CommentOperation.java
+++ sit-wt-runtime/src/main/java/io/sitoolkit/wt/domain/operation/selenium/CommentOperation.java
@@ -1,9 +1,9 @@
 package io.sitoolkit.wt.domain.operation.selenium;
 
-import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Component;
 
+import io.sitoolkit.wt.domain.testscript.Locator;
 import io.sitoolkit.wt.domain.testscript.TestStep;
 
 @Component
@@ -11,11 +11,13 @@ public class CommentOperation extends SeleniumOperation {
 
   @Override
   public void execute(TestStep testStep, SeleniumOperationContext ctx) {
-    if (StringUtils.isEmpty(testStep.getLocator().getValue())) {
-      ctx.info("comment", testStep.getValue());
+    String comment = testStep.getValue();
+    Locator locator = testStep.getLocator();
+    if (locator.getTypeVo() == Locator.Type.na) {
+      ctx.info("comment", comment);
     } else {
-      WebElement element = findElement(testStep.getLocator());
-      ctx.info(element, "comment", testStep.getValue());
+      WebElement element = findElement(locator);
+      ctx.info(element, "comment", comment);
     }
   }
 }
